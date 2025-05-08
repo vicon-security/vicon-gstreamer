7e475d865aad840e064e99b51d2679c644ef8712
smomin7 <smomin@vicon-security.com>
2025-05-08 18:49:16 +0530

    Revert "rtspsrc: Use a flow combiner at the source pads instead of custom logic"
    This reverts commit 835e232e8ca798064cf3526045cbcde94df6820d.

--- subprojects/gst-plugins-good/gst/rtsp/gstrtspsrc.h
+++ subprojects/gst-plugins-good/gst/rtsp/gstrtspsrc.h
@@ -45,7 +45,6 @@
 #define __GST_RTSPSRC_H__
 
 #include <gst/gst.h>
-#include <gst/base/base.h>
 
 G_BEGIN_DECLS
 
@@ -228,8 +227,6 @@ struct _GstRTSPSrc {
   GstSDPMessage   *sdp;
   gboolean         from_sdp;
   GList           *streams;
-  GMutex           flow_combiner_lock;
-  GstFlowCombiner *flow_combiner;
   GstStructure    *props;
   gboolean         need_activate;
 
